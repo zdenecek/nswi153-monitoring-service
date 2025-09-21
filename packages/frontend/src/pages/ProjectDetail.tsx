999005b5cb983828e8d1c1b94beb8aecb417acfe
Zdeněk Tomis <zdnek.tomis@gmail.com>
2025-09-21 22:02:34 +0200

    remove updating

--- packages/frontend/src/pages/ProjectDetail.tsx
+++ packages/frontend/src/pages/ProjectDetail.tsx
@@ -391,15 +391,7 @@ export function ProjectDetail() {
                       <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{monitor.type}</td>
                       <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{monitor.url}</td>
                       <td className="whitespace-nowrap px-3 py-4 text-sm">
-                        {isFetching ? (
-                          <span className="inline-flex items-center rounded-full bg-gray-100 px-2 text-xs font-semibold leading-5 text-gray-800">
-                            <svg className="-ml-0.5 mr-1.5 h-2 w-2 animate-spin text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                            </svg>
-                            UPDATING
-                          </span>
-                        ) : (
+                        {(
                           <span
                             className={`inline-flex rounded-full px-2 text-xs font-semibold leading-5 ${
                               monitor.status === 'up'
